729b85c0e5d3e00df68ebfd89234cd4abbca030c
ansuz <aaronmacsweenmusic@gmail.com>
2014-04-27 20:40:58 -0400

    playing with implementations

--- src/recu.rs
+++ src/recu.rs
@@ -1,6 +1,14 @@
+/*
+    recu.rs
+    demonstrates enums, recursive datatypes, and methdos
+    vanilla
+*/
+
 fn main() {
     let list = ~Node(1, ~Node(2, ~Node(3, ~Empty)));
+    let list2 = ~Node(2, ~Node(3, ~Node(5, ~Empty)));
     println!("Sum of all values in the list: {:i}.", list.sum());
+    println!("Product of all values in the list {:i}.",list2.prod());
 }
 
 enum IntList {
@@ -14,6 +22,12 @@ impl IntList {
         match *self {
             Node(value, next) => value + next.sum(),
             Empty => 0
+        }
+    }
+    fn prod(~self) -> int {
+        match *self {
+            Node(value,next) => value * next.prod(),
+            Empty => 1
         }
     }
 }
