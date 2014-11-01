1c0289e246b113c619bd3bfd782c058930d2444c
flip <ansuz@transitiontech.ca>
2014-11-01 09:25:38 -0400

    pewpew/nesting comments

---
+++ src/pewpew.rs
@@ -0,0 +1,13 @@
+/*  Rust comments are pretty much like C/Javascript comments
+    They can be written across multiple lines, like so */
+
+// This comments everything until the end of the line
+
+/* The big difference, however,
+    /* is that comments in Rust nest */
+     Properly, such that this is still a comment
+*/
+
+fn main(){
+    print!("pewpew!\n");
+}
